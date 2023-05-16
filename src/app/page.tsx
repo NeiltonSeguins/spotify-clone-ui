de06f092f274fed87e9a509cb1004220b54362f1
NeiltonSeguins <seguins.neilton@gmailcom>
2023-05-16 20:00:15 -0300

    Primeiro commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,294 @@
-import Image from 'next/image'
+import { FiHome } from "react-icons/fi";
+import {
+  BsSearch,
+  BsBookmarkHeartFill,
+  BsFillPlayFill,
+  BsHeart,
+} from "react-icons/bs";
+import {
+  MdAddBox,
+  MdOutlineArrowBackIosNew,
+  MdOutlineArrowForwardIos,
+} from "react-icons/md";
+import { BiLibrary } from "react-icons/bi";
+import { SiApplepodcasts } from "react-icons/si";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-56 bg-zinc-950 p-6">
+          <div className="mb-6">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/spotify-logo.png"
+              width={150}
+              height={150}
+              alt="Logo do Spotify"
             />
-          </a>
+          </div>
+          <nav className="space-y-5">
+            <div className="flex items-center gap-3 ">
+              <FiHome />
+              <a href="/" className=" text-xs font-semibold text-zinc-200">
+                Home
+              </a>
+            </div>
+            <div className="flex items-center gap-3 ">
+              <BsSearch />
+              <a href="/" className="text-xs font-semibold text-zinc-200">
+                Search
+              </a>
+            </div>
+            <div className="flex items-center gap-3 ">
+              <BiLibrary />
+              <a href="/" className="text-xs font-semibold text-zinc-200">
+                Your Library
+              </a>
+            </div>
+          </nav>
+
+          <nav className="mt-6 pt-6 flex flex-col gap-4">
+            <div className="flex items-center gap-2 ">
+              <MdAddBox className="w-5 h-5" />
+              <a href="/" className=" text-xs font-semibold text-zinc-200">
+                Create Your Playlist
+              </a>
+            </div>
+            <div className="flex items-center gap-2 ">
+              <BsBookmarkHeartFill className="w-5 h-5 text-indigo-400" />
+              <a href="/" className=" text-xs font-semibold text-zinc-200">
+                Liked Songs
+              </a>
+            </div>
+            <div className="flex items-center gap-2 ">
+              <SiApplepodcasts className="w-5 h-5 text-green-600" />
+              <a href="/" className=" text-xs font-semibold text-zinc-200">
+                Your Episodes
+              </a>
+            </div>
+          </nav>
+
+          <nav className="mt-6 pt-6 flex flex-col gap-1 border-t border-zinc-600">
+            <a href="/" className="text-sm text-zinc-400 hover:text-zinc-100">
+              FAV
+            </a>
+            <a href="/" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Daily Mix 1
+            </a>
+            <a href="/" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Discover Weekly
+            </a>
+            <a href="/" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Malayalam
+            </a>
+            <a href="/" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Dance/EletroMix
+            </a>
+            <a href="/" className="text-sm text-zinc-400 hover:text-zinc-100">
+              EDM/Popular
+            </a>
+          </nav>
+        </aside>
+        <main className="flex-1 p-6">
+          <div className="flex items-center gap-4">
+            <button className="rounded-full bg-black/40 p-2">
+              <MdOutlineArrowBackIosNew />
+            </button>
+            <button className="rounded-full bg-black/40 p-2">
+              <MdOutlineArrowForwardIos />
+            </button>
+          </div>
+
+          <h1 className="font-bold text-3xl mt-10">Good morning</h1>
+
+          <div className="grid grid-cols-4 gap-4 mt-6">
+            <a
+              href="/"
+              className="group bg-white/10 rounded overflow-hidden text-sm flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image
+                src="/coracao.png"
+                width={90}
+                height={90}
+                alt="Imagem de um ícone de coração"
+              />
+              <strong>Liked Songs</strong>
+              <button className="w-10 h-10 flex items-center justify-center pl-0.5 rounded-full bg-green-500 ml-auto mr-8 invisible group-hover:visible">
+                <BsFillPlayFill className="text-zinc-950 w-6 h-6" />
+              </button>
+            </a>
+            <a
+              href="/"
+              className="group bg-white/10 rounded overflow-hidden text-sm flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image
+                src="/neffex.png"
+                width={90}
+                height={90}
+                alt="Imagem de um ícone de coração"
+              />
+              <strong>Neffex Playlist</strong>
+              <button className="w-10 h-10 flex items-center justify-center pl-0.5 rounded-full bg-green-500 ml-auto mr-8 invisible group-hover:visible">
+                <BsFillPlayFill className="text-zinc-950 w-6 h-6" />
+              </button>
+            </a>
+            <a
+              href="/"
+              className="group bg-white/10 rounded overflow-hidden text-sm flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image
+                src="/kda.png"
+                width={90}
+                height={90}
+                alt="Imagem de um ícone de coração"
+              />
+              <strong>K/DA</strong>
+              <button className="w-10 h-10 flex items-center justify-center pl-0.5 rounded-full bg-green-500 ml-auto mr-8 invisible group-hover:visible">
+                <BsFillPlayFill className="text-zinc-950 w-6 h-6" />
+              </button>
+            </a>
+            <a
+              href="/"
+              className="group bg-white/10 rounded overflow-hidden text-sm flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image
+                src="/liked-songs.png"
+                width={90}
+                height={90}
+                alt="Imagem de um ícone de coração"
+              />
+              <strong>Liked Songs</strong>
+              <button className="w-10 h-10 flex items-center justify-center pl-0.5 rounded-full bg-green-500 ml-auto mr-8 invisible group-hover:visible">
+                <BsFillPlayFill className="text-zinc-950 w-6 h-6" />
+              </button>
+            </a>
+            <a
+              href="/"
+              className="group bg-white/10 rounded overflow-hidden text-sm flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image
+                src="/dance.png"
+                width={90}
+                height={90}
+                alt="Imagem de um ícone de coração"
+              />
+              <strong>Dance/Electronic Mix</strong>
+              <button className="w-10 h-10 flex items-center justify-center pl-0.5 rounded-full bg-green-500 ml-auto mr-8 invisible group-hover:visible">
+                <BsFillPlayFill className="text-zinc-950 w-6 h-6" />
+              </button>
+            </a>
+          </div>
+
+          <h2 className="font-bold text-2xl mt-10">Shows you might like</h2>
+
+          <div className="grid grid-cols-6 gap-4 mt-4">
+            <a
+              href="/"
+              className="bg-white/5 p-3 rounded hover:bg-white/10 flex flex-col gap-1"
+            >
+              <Image
+                className="w-full"
+                src="/capa1.png"
+                width={231}
+                height={288}
+                alt="Imagem do album"
+              />
+              <strong className="font-semibold">Weekly Motivation</strong>
+              <span className="text-sm text-zinc-500">Ben Ina Scott</span>
+            </a>
+            <a
+              href="/"
+              className="bg-white/5 p-3 rounded hover:bg-white/10 flex flex-col gap-1"
+            >
+              <Image
+                className="w-full"
+                src="/capa2.png"
+                width={231}
+                height={288}
+                alt="Imagem do album"
+              />
+              <strong>Weekly Motivation</strong>
+              <span className="text-sm text-zinc-500">Ben Ina Scott</span>
+            </a>
+            <a
+              href="/"
+              className="bg-white/5 p-3 rounded hover:bg-white/10 flex flex-col gap-1"
+            >
+              <Image
+                className="w-full"
+                src="/capa3.png"
+                width={231}
+                height={288}
+                alt="Imagem do album"
+              />
+              <strong>Weekly Motivation</strong>
+              <span className="text-sm text-zinc-500">Ben Ina Scott</span>
+            </a>
+            <a
+              href="/"
+              className="bg-white/5 p-3 rounded hover:bg-white/10 flex flex-col gap-1"
+            >
+              <Image
+                className="w-full"
+                src="/capa4.png"
+                width={231}
+                height={288}
+                alt="Imagem do album"
+              />
+              <strong>Weekly Motivation</strong>
+              <span className="text-sm text-zinc-500">Ben Ina Scott</span>
+            </a>
+            <a
+              href="/"
+              className="bg-white/5 p-3 rounded hover:bg-white/10 flex flex-col gap-1"
+            >
+              <Image
+                className="w-full"
+                src="/capa5.png"
+                width={231}
+                height={288}
+                alt="Imagem do album"
+              />
+              <strong>Weekly Motivation</strong>
+              <span className="text-sm text-zinc-500">Ben Ina Scott</span>
+            </a>
+            <a
+              href="/"
+              className="bg-white/5 p-3 rounded hover:bg-white/10 flex flex-col gap-1"
+            >
+              <Image
+                className="w-full"
+                src="/capa6.png"
+                width={231}
+                height={288}
+                alt="Imagem do album"
+              />
+              <strong>Weekly Motivation</strong>
+              <span className="text-sm text-zinc-500">Ben Ina Scott</span>
+            </a>
+          </div>
+        </main>
+      </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-4 flex items-center justify-between">
+        <div className="flex items-center gap-3 w-1/6">
+          <Image
+            src="/capa-player.png"
+            width={60}
+            height={60}
+            alt="Capa da música que está tocando"
+          />
+          <div className="flex flex-col">
+            <strong className="font-normal">Dreaming On</strong>
+            <span className="text-xs text-zinc-400">NEFFEX</span>
+          </div>
+          <button className="ml-auto mr-8">
+            <BsHeart />
+          </button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <div></div>
+        <div></div>
+      </footer>
+    </div>
+  );
 }
